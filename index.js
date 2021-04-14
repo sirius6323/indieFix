f9aec42e0b8b423b538398c9439712d1b46b51ba
sirius6323 <sirius6323@gmail.com>
2021-04-13 22:38:34 -0700

    If applied, updated comment on line 1 and added error handling

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// Import Express
+// Import Express, Morgan, bodyParser, methodOverride
 const express = require('express'),
 	morgan = require('morgan'),
-	app = express();
+	app = express(),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override');
 
 // Movies array to test functionality
 let top10Movies = [
@@ -21,6 +23,12 @@ app.use(morgan('common'));
 // Serves static documentation of indieFix
 app.use(express.static('public'));
 
+// Error handling middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
 // GET requests
 app.get('/', (req, res) => {
 	res.send('Welcome to indieFix!');
@@ -28,6 +36,11 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
 	res.json(top10Movies);
+});
+
+app.use((err, req, res, next) => {
+	console.log(err.stack);
+	res.status(500).send('You broke something!!! Call me to fix it.');
 });
 
 // Listens for requests
