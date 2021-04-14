4754bb09c0f211f3804dde9eedce1de97fbbaae3
sirius6323 <sirius6323@gmail.com>
2021-04-13 21:32:56 -0700

    If applied, removed code and added response welcome message

--- index.js
+++ index.js
@@ -2,31 +2,11 @@
 const express = require('express'),
 	app = express();
 
-// Top 10 Movies
-let top10Movies = [
-	{
-		title: 'Get Out',
-		director: 'Jordan Peele',
-	},
-	{
-		title: 'Parasite',
-		director: 'Bong Joon Ho',
-	},
-	{
-		title: 'Momento',
-		director: 'Christoper Nolan',
-	},
-	{
-		title: 'The Blair Witch Project',
-		director: 'Eduardo Sanchez, Daniel Myrick',
-	},
-	{
-		title: 'Reservoir Dogs',
-		director: 'Quentin Tarantino',
-	},
-];
-
 // GET requests
+app.get('/', (req, res) => {
+	res.send('Welcome to indieFix!');
+});
+
 app.get('/movies', (req, res) => {
 	res.json(top10Movies);
 });
