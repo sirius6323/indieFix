21a66bf4dd26484b65b89cb244f0158ff2631859
sirius6323 <sirius6323@gmail.com>
2021-04-13 21:25:48 -0700

    If applied, added movies to array of top10Movies

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 // Import Express
 const express = require('express'),
 	app = express();
+
+// Top 10 Movies
+let top10Movies = [
+	{
+		title: 'Get Out',
+		director: 'Jordan Peele',
+	},
+	{
+		title: 'Parasite',
+		director: 'Bong Joon Ho',
+	},
+	{
+		title: 'Momento',
+		director: 'Christoper Nolan',
+	},
+	{
+		title: 'The Blair Witch Project',
+		director: 'Eduardo Sanchez, Daniel Myrick',
+	},
+	{
+		title: 'Reservoir Dogs',
+		director: 'Quentin Tarantino',
+	},
+];
+
+// GET requests
+app.get('/movies', (req, res) => {
+	res.json(top10Movies);
+});
