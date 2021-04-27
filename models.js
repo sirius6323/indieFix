8bf581839f3e28a14dfd9444f0c53a36c60f2482
sirius6323 <sirius6323@gmail.com>
2021-04-26 18:52:20 -0700

    If applied, added movie and user schema to models.js

---
+++ models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Movie Schema
+let movieSchema = mongoose.Schema({
+	Title: { type: String, required: true },
+	Description: { type: String, required: true },
+	Genre: {
+		Name: String,
+		Description: String,
+	},
+	Director: {
+		Name: String,
+		Bio: String,
+		Birth: String,
+	},
+	ImagePath: String,
+	Featured: Boolean,
+});
+
+// User Schema
+let userSchema = mongoose.Schema({
+	FirstName: { type: String, required: true },
+	LastName: { type: String, required: true },
+	Birthday: Date,
+	Username: { type: String, required: true },
+	Password: { type: String, required: true },
+	Email: { type: String, required: true },
+	FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
