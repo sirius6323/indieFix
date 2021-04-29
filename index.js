38371a6a85e11da416ad50cb7657ba47141b32af
sirius6323 <sirius6323@gmail.com>
2021-04-29 13:55:26 -0700

    If applied, allows access to domains within the variable allowedOrigins

--- index.js
+++ index.js
@@ -53,7 +53,18 @@ app.use(bodyParser.json());
 
 let auth = require('./auth')(app);
 
-app.use(cors());
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				let message = `The CORS policy for this application doesn't allow access from origin: ${origin}`;
+				return callback(new Error(message), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 app.use((err, req, res, next) => {
 	console.log(err.stack);
