4e7739f8fac8184396a23ca7aab77292cf1dc587
sirius6323 <sirius6323@gmail.com>
2021-04-29 15:30:29 -0700

    If applied, added bcrypt, Hashing, and Validating Password

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+	bcrypt = require('bcrypt');
 
 // Movie Schema
 const movieSchema = mongoose.Schema({
@@ -28,6 +29,14 @@ const userSchema = mongoose.Schema({
 	FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 	WatchListMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
+
+userSchema.statics.hashPassword = (password) => {
+	return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+	return bcrypt.compareSync(password, this.Password);
+};
 
 const Movie = mongoose.model('Movie', movieSchema);
 const User = mongoose.model('User', userSchema);
