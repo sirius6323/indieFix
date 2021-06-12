e0f779bf28ac5cd1ad09e42cad5570d5a78a99e1
sirius6323 <sirius6323@gmail.com>
2021-06-11 20:58:31 -0700

    Changed to allow all origins

--- index.js
+++ index.js
@@ -60,19 +60,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // Allows access to indieFix API if domain in within allowedOrigins
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			// If specific origin isn't found on the list of allowed origins return message
-			if (allowedOrigins.indexOf(origin) === -1) {
-				let message = `The CORS policy for this application doesn't allow access from origin: ${origin}`;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+app.use(cors());
 
 /*
 mongoose.connect('mongodb://localhost:27017/indieFixDB', {
