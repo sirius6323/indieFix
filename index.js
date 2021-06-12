794887bf9f9b63fb3afd45282f8b9ad76c89c8b0
sirius6323 <sirius6323@gmail.com>
2021-06-11 22:30:31 -0700

    Refactored codebase for CORS

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const express = require('express'),
 	bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
 // Error handling middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const auth = require('./auth')(app);
 
