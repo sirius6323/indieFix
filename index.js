49d64f2372fe62e3f523839a58f697ea073d4e98
sirius6323 <sirius6323@gmail.com>
2021-06-11 23:52:07 -0700

    Removed unused code

--- index.js
+++ index.js
@@ -59,15 +59,6 @@ mongoose.connect('mongodb://localhost:27017/indieFixDB', {
 });
 */
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
-
 app.use((err, req, res, next) => {
 	console.log(err.stack);
 	res
